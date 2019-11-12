5c9c405f31a9c2e5127224e487ec71c773110c23
Keshav612 <500062098@stu.upes.ac.in>
2019-11-12 10:14:33 +0530

    introductory phase

---
+++ leap.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main()
+{
+       printf("\t\t\t\t********* W E L C O M E ********* " );
+       printf(" \n\t\t\t\t U P E S  N O O B S "  );
+      printf(" \n\t\t\t\t M E D I C A L   D I A G N O S I S   S O F T W A R E  ");
+      int x;
+      
+    int age;
+    float ht;
+    int wt;
+    char name[40];
+
+     	char sex;
+     printf(" \n \tP E R S O N A L   I N F O R M A T I O N");
+    
+     printf("\n\tA G E          :");
+     scanf("%d",&age);
+     printf("\n\tN A M E :");
+     scanf("%s",&name);
+     printf("\n\tW E I G H T    :");
+	 	scanf("%d",&wt);
+     printf("\n\tH E I G H T    :");
+     scanf("%f",&ht);
+     printf("\n\tS E X (M/F)    :");    
+	scanf("%d",&sex);
+	printf(" \n\t\t\t**  D I A G N O S I S   W I N D O W **   ");
+     printf("\n\n\n");
+     printf(" \t\t\tLet's have a look at  symptoms.........");
+     printf("\n\n\n\n\n\n\t\t\t ");
+     
+}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
